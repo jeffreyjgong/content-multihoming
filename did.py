7e1aa1ba76deb16baefc7952474812457671994b
AlexShin <alex.shin@yale.edu>
2023-11-30 19:13:04 -0500

    did and rse class

--- did.py
+++ did.py
@@ -1,1 +1,15 @@
-print("hello")
+class DID:
+    def __init__(self, did, size, replicationRule):
+        self.did = did
+        self.size = size
+        self.replicationRule = replicationRule
+
+class RSE:
+    def __init__(self, name, capacity, dids):
+        self.name = name
+        self.capacity = capacity
+        self.dids = dids
+        self.capacityLeft = capacity - sum([did.size for did in dids])
+        self.capacityUsed = sum([did.size for did in dids])
+        self.cloudCostModel = None
+
